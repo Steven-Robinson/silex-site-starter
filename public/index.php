5d701789f6564cd5bb7f6ee698818f013a6846a2
Steven Robinson <steverobinson1978@gmail.com>
2016-01-07 15:34:58 +0000

    Registered the service controller service provider and added a controller class.

--- public/index.php
+++ public/index.php
@@ -1,11 +1,25 @@
 <?php
 
+use App\Controller\HomeController;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function () {
-    return 'Hello World!';
+// ToDo: Move all the following into a bootstrap class
+
+// register services
+
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+
+// register container entries
+
+$app['home.controller'] = $app->share(function() {
+    return new HomeController();
 });
+
+// register routes
+
+$app->get('/', "home.controller:indexAction");
 
 $app->run();
